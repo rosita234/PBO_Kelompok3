b970c847ff70a080dd642a1f5641511431d53f6e
Edbert395 <edbert.muis@gmail.com>
2023-05-24 20:42:11 +0700

    Update Edbert.java

--- kelompok3/OOP/src/Edbert.java
+++ kelompok3/OOP/src/Edbert.java
@@ -1,5 +1,5 @@
 public class Edbert {
     public static void main(String[] args) {
-        System.out.println("Nama/NIM: Edbert Muis, 03081220022");
+        System.out.println("Nama,NIM: Edbert Muis,03081220022");
     }
 }
