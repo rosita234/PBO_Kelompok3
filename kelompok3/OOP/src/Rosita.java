065c4a9db06e34f04a25ce8dd5a92759e1db986a
rosita234 <03081220014@student.uph.edu>
2023-05-24 19:32:08 +0700

    Create Rosita.java

---
+++ kelompok3/OOP/src/Rosita.java
@@ -0,0 +1,6 @@
+public class Rosita {
+    public static void main(String[] args) {
+        System.out.println("Nama\t: Rosita Darianty");
+        System.out.println("NIM\t: 03081220014 ");
+    }
+}
