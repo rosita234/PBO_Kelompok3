37dc8b8fd8a631ef1d825d902e2df2e34b89f35e
StephanieWgs <126796594+StephanieWgs@users.noreply.github.com>
2023-05-27 14:59:51 +0700

    Update App & Stock

--- Indomay/src/App.java
+++ Indomay/src/App.java
@@ -7,9 +7,29 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         do{
+            showMenu();
+            System.out.println("Masukkan pilihan :");
+            String pilihan = sc.nextLine();
+            
+            switch(pilihan){
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                default:
+                    break;
+            }
 
+            sc.close();
         }while(choice);
     }
 
-
+    public static void showMenu(){
+        System.out.println("1. Scan barang");
+        System.out.println("2. Cek Stok");
+        System.out.println("3. Cek Data Pembeli");
+        System.out.println("4. History Transaksi");
+        System.out.println("5. Exit");
+    }
 }
--- Indomay/src/Stok.java
+++ Indomay/src/Stok.java
@@ -1,3 +1,9 @@
+/*Class stok dapat digunakan untuk
+- Input Stok
+- Edit Stok (Menambah, mengubah, menghapus stok)
+- Cek Status Stok, termasuk jumlah barang 
+ */
+
 public class Stok {
     
 }
