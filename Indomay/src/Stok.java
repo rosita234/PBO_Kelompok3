2e3f8fdd7a27bc60824c833c410e00ef002c70bb
StephanieWgs <126796594+StephanieWgs@users.noreply.github.com>
2023-05-29 21:57:46 +0700

    Update Stok.java

--- Indomay/src/Stok.java
+++ Indomay/src/Stok.java
@@ -5,5 +5,47 @@
  */
 
 public class Stok {
+    private String kodeStok;
+    private String namaStok;
+    private int jumlahStok;
+    private int hargaStok;
+
+    public Stok(String kodeStok, String namaStok, int jumlahStok, int hargaStok){
+        this.kodeStok = kodeStok;
+        this.namaStok = namaStok;
+        this.jumlahStok = jumlahStok;
+        this.hargaStok = hargaStok;
+    }
+
+    public int getHargaStok() {
+        return hargaStok;
+    }
+
+    public int getJumlahStok() {
+        return jumlahStok;
+    }
+
+    public String getKodeStok() {
+        return kodeStok;
+    }
     
+    public String getNamaStok() {
+        return namaStok;
+    }
+
+    public void setHargaStok(int hargaStok) {
+        this.hargaStok = hargaStok;
+    }
+
+    public void setJumlahStok(int jumlahStok) {
+        this.jumlahStok = jumlahStok;
+    }
+
+    public void setKodeStok(String kodeStok) {
+        this.kodeStok = kodeStok;
+    }
+
+    public void setNamaStok(String namaStok) {
+        this.namaStok = namaStok;
+    }
 }
