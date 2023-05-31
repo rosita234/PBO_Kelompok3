dff44b34faa4dfa664954ad3d393f57b0ba9632d
rosita234 <03081220014@student.uph.edu>
2023-05-31 19:00:27 +0700

    Update History.java

--- Indomay/src/History.java
+++ Indomay/src/History.java
@@ -1,3 +1,6 @@
+//fitur ini digunakan untuk mengecek history dari setiap transakasi
+//By: Rosita Darianty(03081220014)
+//email: 03081220014@student.uph.edu
 public class History {
-    //fitur ini digunakan untuk mengecek history dari setiap transakasi
+    
 }
