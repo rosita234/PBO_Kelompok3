ac394dff789c91a1d5a57c3a451c8c65977f9c8c
AlfredoJudd <03081220011@student.uph.edu>
2023-05-31 20:41:38 +0700

    Update Staff Setter Getter
    PT 4

--- kelompok3/OOP/src/Alfredo.java
+++ kelompok3/OOP/src/Alfredo.java
@@ -3,3 +3,30 @@ public class Alfredo {
         System.out.println("Hello, ini Alfredo!");
     }
 }
+/*
+public class StaffNode {
+    private Staff user;
+    private StaffNode next;
+
+    public StaffNode(Staff user) {
+        this.user = user;
+        this.next = null;
+    }
+    //Operasi memanggil Staff
+    public Staff getStaff() {
+        return user;
+    }
+    //Operasi memanggil Node berikutnya
+    public StaffNode getNext() {
+        return next;
+    }
+    //Operasi menggubah Staff
+    public void setUser(Staff user) {
+        this.user = user;
+    }
+    //Operasi menggubah Node berikutnya
+    public void setNext(StaffNode next) {
+        this.next = next;
+    }
+}
+ */
