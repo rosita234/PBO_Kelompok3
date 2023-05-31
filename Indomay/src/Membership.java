c6f853759aae2e58a09fc14cd00eee0b15ee254a
AurelyaHQ <ayaya898@gmail.com>
2023-05-31 20:07:49 +0700

    Update Membership Setter Getter

--- Indomay/src/Membership.java
+++ Indomay/src/Membership.java
@@ -7,4 +7,28 @@ public class Membership {
      * - Cek poin membership
      * - Cek promo (benefit membership)
      */
+
+    private int poin;
+    private int promo;
+
+    public Membership(int poin, int promo){
+        this.poin = poin;
+        this.promo = promo;
+    }
+
+    public int getPoin() {
+        return poin;
+    }
+
+    public int getPromo() {
+        return promo;
+    }
+
+    public void setPoin(int poin) {
+        this.poin = poin;
+    }
+
+    public void setPromo(int promo) {
+        this.promo = promo;
+    }
 }
